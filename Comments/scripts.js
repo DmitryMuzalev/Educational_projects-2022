e9b2ab2ec8e1b6d543822380a85f14ebfbef42d7
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-07-06 12:14:11 +0300

    feat: create html project structure

---
+++ Comments/scripts.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    id: 1,
+    fullName: "Priscilla Shelton",
+    img: "../src/images/Priscilla Shelton.jpg",
+    profession: "Model",
+    comment:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or words which don't look even slightly believable.",
+  },
+  {
+    id: 2,
+    fullName: "Ella Floyd",
+    img: "../src/images/Ella Floyd.jpg",
+    profession: "Veterinarian",
+    comment:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+  },
+  {
+    id: 3,
+    fullName: "Jewel Harmon",
+    img: "../src/images/Jewel Harmon.jpg",
+    profession: "Cameraman",
+    comment:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    id: 4,
+    fullName: "Joanna Ballard",
+    img: "../src/images/Joanna Ballard.jpg",
+    profession: "Psychiatrist",
+    comment:
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound.",
+  },
+  {
+    id: 5,
+    fullName: "Tristan Lamb",
+    img: "../src/images/Tristan Lamb.jpg",
+    profession: "Accountant",
+    comment:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or words which don't look even slightly believable. ",
+  },
+  {
+    id: 6,
+    fullName: "Mason Montgomery",
+    img: "../src/images/Mason Montgomery.jpg",
+    profession: "Insurance agent",
+    comment:
+      "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+  },
+];
